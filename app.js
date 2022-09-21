8355ce95bdb75901c8126a877e4fadcc00a8d255
Jarrian <glorianagojar@gmail.com>
2022-09-21 15:39:25 +0800

    update media query

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 window.addEventListener('scroll', function() {
 	var scrollIn = document.querySelectorAll('.s1');
 
+  //  Partial
+  //  pos.top < window.innerHeight && pos.bottom >= 0
+
+  // Full
+  // pos.top >= 0 && pos.bottom <= window.innerHeight
+
   scrollIn.forEach((e) => {
     const pos = e.getBoundingClientRect();
 
-    if(pos.top >= 0 && pos.bottom <= window.innerHeight) {
+    if(pos.top * 2 >= 0 && pos.bottom/2 <= window.innerHeight) {
       e.classList.add('scroll-in');
       console.log('visible');
     }
