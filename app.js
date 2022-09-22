6835e7f836f69676c78251a1e6bc020ca6873bd1
Jarrian <glorianagojar@gmail.com>
2022-09-22 12:28:41 +0800

    industry media query updated

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 window.addEventListener('scroll', function() {
-	var scrollIn = document.querySelectorAll('.s1');
 
   //  Partial
   //  pos.top < window.innerHeight && pos.bottom >= 0
@@ -7,12 +6,53 @@ window.addEventListener('scroll', function() {
   // Full
   // pos.top >= 0 && pos.bottom <= window.innerHeight
 
-  scrollIn.forEach((e) => {
+  const fade1 = document.querySelectorAll('.fade-1');
+
+  fade1.forEach((e) => {
     const pos = e.getBoundingClientRect();
 
-    if(pos.top >= 0 && pos.bottom/1.5 <= window.innerHeight) {
-      e.classList.add('scroll-in');
-      console.log('visible');
+    if(pos.top >= 0 && pos.bottom/2 <= window.innerHeight) {
+      e.classList.remove('fade-1');
+    }
+  });
+
+  const s1A = document.querySelectorAll('.s1-a');
+
+  s1A.forEach((e) => {
+    const pos = e.getBoundingClientRect();
+
+    if(pos.top >= 0 && pos.bottom/2 <= window.innerHeight) {
+      e.classList.remove('s1-a');
+    }
+  });
+
+  const s1B = document.querySelectorAll('.s1-b');
+
+  s1B.forEach((e) => {
+    const pos = e.getBoundingClientRect();
+
+    if(pos.top >= 0 && pos.bottom/2 <= window.innerHeight) {
+      e.classList.remove('s1-b');
+    }
+  });
+
+  const s2A = document.querySelectorAll('.s2-a');
+
+  s2A.forEach((e) => {
+    const pos = e.getBoundingClientRect();
+
+    if(pos.top >= 0 && pos.bottom <= window.innerHeight) {
+      e.classList.remove('s2-a');
+    }
+  });
+
+  const s2B = document.querySelectorAll('.s2-b');
+
+  s2B.forEach((e) => {
+    const pos = e.getBoundingClientRect();
+
+    if(pos.top >= 0 && pos.bottom <= window.innerHeight) {
+      e.classList.remove('s2-b');
     }
   });
 });
---
+++ assets/style.md
@@ -0,0 +1,40 @@
+:root {
+  --clr-1-a: #8480ff;
+  --clr-1-b: #a6a3ff;
+  --clr-1-c: #524dc0;
+  --clr-2-a: #2499ff;
+  --clr-2-b: #66b8ff;
+  --clr-2-c: #0080f0;
+  --clr-3-a: #2cd896;
+  --clr-3-b: #6ce5b6;
+  --clr-3-c: #1b9868;
+  --clr-4-a: #ffd500;
+  --clr-4-b: #ffe14d;
+  --clr-4-c: #ccaa00;
+  --clr-5-a: #ff8800;
+  --clr-5-b: #ffac4d;
+  --clr-5-c: #cc6d00;
+  --clr-6-a: #b0b0b0;
+  --clr-6-b: #e0e0e0;
+  --clr-6-c: #808080;
+
+  --brown-1: #2b2b2b;
+  --brown-2: hsl(0, 0%, 8%);
+
+  --dark-1: #001122;
+
+  --white-1: #ffffff;
+  --white-2: #e0e0e0;
+  --white-3: #b0b0b0;
+  --white-4: #808080;
+  --white-5: #606060;
+  --white-6: #404040;
+  --white-7: #202020;
+  --white-8: #101010;
+
+  --lines-1: #131746;
+
+  --header-bg-clr: #66b8ffff;
+
+  --text-clr: white;
+}
